c6c14a9c875eeb562f53723987dbd847165a2998
JuanMZ96 <lorjan96@gmail.com>
2023-01-19 19:24:06 -0300

    Update

--- WebScraping/Scrap.py
+++ WebScraping/Scrap.py
@@ -2,9 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url = 'https://argentina.as.com/resultados/futbol/primera/clasificacion/'
+url = 'https://resultados.as.com/resultados/futbol/primera/clasificacion/'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content,'html.parser')
 
-eq
+eq =  soup.find_all('span', class_= 'nombre-equipo')
+
+#print(eq)
+for val in eq:
+    print(val)
